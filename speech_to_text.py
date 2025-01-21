0451d88ba898b6f02996e3693c817caa03920f7e
M Asad <muhammadasadwork28@gmail.com>
2025-01-21 16:55:16 +0500

    nova added as a translator to speech

--- speech_to_text.py
+++ speech_to_text.py
@@ -8,8 +8,7 @@ client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
 def speech_to_text(text:str):
     response = client.audio.speech.create(
         model="tts-1",
-        voice="alloy",
+        voice="nova",
         input=text,
     )
-
     return response.stream_to_file("output.mp3")
